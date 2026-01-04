9c4c4da73c8dd4b1af9f7496f91616ddec627bb0
otaviosilva2024 <otavioaugustoheglersilva@gmail.com>
2026-01-04 17:34:25 -0300

    Update http.py

--- gallery_dl/downloader/http.py
+++ gallery_dl/downloader/http.py
@@ -6,7 +6,7 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 
-"""Downloader module for http:// and https:// URLs"""
+"""Downloader module for https://www.deviantart.com/n8navarro/art/John-Corben-Metallo-New-Earth-1212585450 and https:// URLs"""
 
 import time
 import mimetypes
